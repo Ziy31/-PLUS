84a161a5678a61f21346f833e88cd5b28b9398e8
Алмазия Кутдусова <kutdusova17ai@gmail.com>
2025-02-08 13:57:54 +0300

    Была добавлена метод для перемешивания карт подземелья

--- Числовой квест PLUS/Program.cs
+++ Числовой квест PLUS/Program.cs
@@ -10,6 +10,19 @@ namespace Числовой_квест_PLUS
     {
         static void Main(string[] args)
         {
+
+        }
+
+        // Метод для перемешивания массива (карты подземелья)
+        static void ShuffleArray(string[] array, Random random)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = random.Next(i, array.Length); // Генерация случайного индекса для перемешивания
+                string temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
         }
     }
 }
